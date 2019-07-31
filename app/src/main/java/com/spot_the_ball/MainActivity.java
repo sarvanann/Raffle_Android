149da0b9fa6198aaa17656e5e49923b26b8f2359
saravanansivardm <sivasaravananrdm@gmail.com>
2019-07-31 15:37:24 +0530

    Initial Commit.

---
+++ app/src/main/java/com/spot_the_ball/MainActivity.java
@@ -0,0 +1,13 @@
+package com.spot_the_ball;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
