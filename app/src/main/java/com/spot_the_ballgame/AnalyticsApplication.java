87cc565aec8126d1ffcff195611a23079535ef0f
saravanansivardm <sivasaravananrdm@gmail.com>
2019-11-26 15:30:53 +0530

    This java class used google analytics for developing and monitoring application.

---
+++ app/src/main/java/com/spot_the_ballgame/AnalyticsApplication.java
@@ -0,0 +1,40 @@
+package com.spot_the_ballgame;
+
+import android.app.Application;
+
+import com.google.android.gms.analytics.GoogleAnalytics;
+import com.google.android.gms.analytics.Tracker;
+import com.onesignal.OneSignal;
+
+public class AnalyticsApplication extends Application {
+
+    private static GoogleAnalytics sAnalytics;
+    private static Tracker sTracker;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        sAnalytics = GoogleAnalytics.getInstance(this);
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init();
+    }
+
+    /**
+     * Gets the default {@link Tracker} for this {@link Application}.
+     *
+     * @return tracker
+     */
+    synchronized public Tracker getDefaultTracker() {
+        // To enable debug logging use: adb shell setprop log.tag.GAv4 DEBUG
+        if (sTracker == null) {
+            sTracker = sAnalytics.newTracker(R.xml.global_tracker);
+        }
+
+        return sTracker;
+    }
+}
